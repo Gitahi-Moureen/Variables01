95de230a1d3681925c885dda43b1cae24048c3da
Gitahi-Moureen <moureengitahiwangari@gmail.com>
2024-02-13 20:21:33 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var fullName = "Moureen Gitahi"
+    var age = 20
+    var phoneNumber = "0717244476"
+    var weightInKg = 52.6
+    val citizen = true
+    println(fullName)
+    println(age)
+    println(phoneNumber)
+    println(weightInKg)
+    println(citizen
